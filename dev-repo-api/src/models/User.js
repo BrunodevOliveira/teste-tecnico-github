a5792656a729357f063fc27b1380f2a4a255bc30
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-07-07 12:10:15 -0300

    Criação dos models User e Reository

---
+++ dev-repo-api/src/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+// Definimos a estrutura padrão do documento
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      // Usará o email como Chave única, garantindo que não teremos dois e-mails iguais
+      index: {
+        unique: true
+      },
+    },
+
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+
+  // 2º parte - Configuração do model
+  {
+    timestamps: true //insere a data de criação e atualização do registro
+  }
+  
+)
+
+export default mongoose.model('User', userSchema) //Construo o model passando como argumento o nome e seu esquema 
